94af9b6eeff86dd55df3f18b48779742cebbe26b
Alexander <jinmaster707@gmail.com>
2021-04-20 14:51:47 +0600

    Added button components and inputs, as well as styles for them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import {Component} from 'react';
+import './App.css';
 
+class ButtonNum extends Component {
+  handleClick = () => {
+    console.log(this);
+  }
+  render(){
+    return <button className='button-number' onClick={this.handleClick} >{this.props.num}</button>;
+  }
+}
+class ButtonOperation extends Component{
+  render(){
+    return (
+      <button className='button-operation'>{this.props.operation}</button>
+    )
+  }
+}
+class InputNum extends Component{
+  render(){
+    return (
+      <input type='text' className='input-number' placeholder='0'></input>
+    )
+  }
+}
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <InputNum></InputNum>
+      <ButtonNum num='0'></ButtonNum>
+      <ButtonNum num='1'></ButtonNum>
+      <ButtonNum num='2'></ButtonNum>
+      <ButtonNum num='3'></ButtonNum>
+      <ButtonNum num='4'></ButtonNum>
+      <ButtonNum num='5'></ButtonNum>
+      <ButtonNum num='6'></ButtonNum>
+      <ButtonNum num='7'></ButtonNum>
+      <ButtonNum num='8'></ButtonNum>
+      <ButtonNum num='9'></ButtonNum>
+      <ButtonNum num='.'></ButtonNum>
+      <ButtonOperation operation='+'></ButtonOperation>
     </div>
   );
 }
