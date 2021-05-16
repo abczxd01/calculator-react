fbbb71f1eb60d36b8aa2d5d904a3a51391b2d851
Alexander <jinmaster707@gmail.com>
2021-05-16 16:39:18 +0500

    Added test calculation and changed addition elements

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function ButtonNum(props) {
 }
 function ButtonOperation(props) {
   return (
-    <button 
-    className={`${props.className ?? ""} button-operation`}
-    onClick={() => props.handleClick(props.operation)}
+    <button
+      className={`${props.className ?? ""} button-operation`}
+      onClick={() => props.handleClick(props.operation)}
     >
       {props.operation}
     </button>
@@ -30,6 +30,9 @@ function InputField(props) {
       placeholder="0"
       value={props.value}
       onChange={props.handleChange}
+      onKeyPress={(event) => {
+        if (event.key === "Enter") props.calculate();
+      }}
     />
   );
 }
@@ -37,55 +40,78 @@ function InputField(props) {
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,'.'];
-    this.operations = ["+", "-", "×", "÷", "%", "С", "="];
+    this.numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "."];
+    this.operations = ["+", "±", "−", "×", "÷", "%", "C", "="];
     this.state = {
       mathExpression: "",
     };
   }
+  calculate = () => {
+    this.setState((state) => ({
+      mathExpression: `${eval(state.mathExpression)}`,
+    }));
+  };
   handleChange = (e) => {
     this.setState({ mathExpression: e.currentTarget.value });
   };
   handleClick(value) {
-    this.setState((state, props) => ({
-      mathExpression: `${state.mathExpression}${value}`,
-    }));
+    switch (value) {
+      case "C":
+        let mathExpression = this.state.mathExpression.split("");
+        mathExpression.pop();
+        mathExpression = mathExpression.join("");
+        this.setState({
+          mathExpression: `${mathExpression}`,
+        });
+        break;
+      case "=":
+        this.calculate();
+        break;
+      default:
+        this.setState((state, props) => ({
+          mathExpression: `${state.mathExpression}${value}`,
+        }));
+        break;
+    }
   }
   render() {
     return (
       <div className="calculator">
         <InputField
+          calculate={this.calculate}
           handleChange={this.handleChange}
           value={this.state.mathExpression}
         ></InputField>
-        {this.numbers.map((number) => {
-          return (
-            <ButtonNum
-              num={number}
-              key={number}
-              handleClick={(num) => this.handleClick(num)}
-            ></ButtonNum>
-          );
-        })}
-        {this.operations.map((operation) => {
-          if (operation === "=") {
+        <div className="calculator__buttons">
+          {this.numbers.map((number) => {
+            return (
+              <ButtonNum
+                num={number}
+                key={number}
+                handleClick={(num) => this.handleClick(num)}
+              ></ButtonNum>
+            );
+          })}
+          {this.operations.map((operation) => {
+            if (operation === "=") {
+              return (
+                <ButtonOperation
+                  key={operation}
+                  operation={operation}
+                  className="button-equals"
+                  handleClick={(operation) => this.handleClick(operation)}
+                ></ButtonOperation>
+              );
+            }
             return (
               <ButtonOperation
                 key={operation}
                 operation={operation}
-                className="button-equals"
                 handleClick={(operation) => this.handleClick(operation)}
               ></ButtonOperation>
             );
-          }
-          return (
-            <ButtonOperation
-              key={operation}
-              operation={operation}
-              handleClick={(operation) => this.handleClick(operation)}
-            ></ButtonOperation>
-          );
-        })}
+          })}
+        </div>
       </div>
     );
   }
