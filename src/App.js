94298611728bd49325d5cef8a58ab310bf30e4ac
Alexander <jinmaster707@gmail.com>
2021-05-21 11:51:00 +0500

    Added history switch,errormesage and expression validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import Toggle from "./Toggle/Toggle";
 import "./App.css";
 import calculate from "./calculate";
 
+function ButtonsArrow(props) {
+  return (
+    <div className="buttons-arrow">
+      <button
+        className="button-arrow button-arrow-left"
+        onClick={props.handleClick}
+      >
+        {"<"}
+      </button>
+      <button
+        className="button-arrow button-arrow-right"
+        onClick={props.handleClick}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+}
 function ButtonNum(props) {
   return (
     <button
@@ -29,17 +47,17 @@ function InputField(props) {
       type="text"
       className="input-field"
       placeholder="0"
-      // readOnly
       value={props.value}
       onChange={props.handleChange}
-      onKeyPress={(event) => {
-        if (event.key === "Enter")
-          console.log(calculate(event.currentTarget.value));
-      }}
+      onKeyPress={(event) =>
+        event.key === "Enter" ? props.calculateExpression() : null
+      }
     />
   );
 }
-
+function MessageErrorExpression(props) {
+  return <p className="error-expression">{props.message}</p>;
+}
 class Calculator extends Component {
   constructor(props) {
     super(props);
@@ -49,61 +67,146 @@ class Calculator extends Component {
       history: [
         {
           mathExpression: "",
-          mathExpressionResult: "",
+          result: "",
         },
       ],
+      mathExpressionError: "",
       mathExpression: "",
+      historyStep: 0,
     };
   }
-  handleChange = (e) => {
-    this.setState({ mathExpression: e.currentTarget.value });
+  validateMathExpression() {
+    const reg = {
+      exception: /[^0-9+/:÷*×−.,-]/i,
+      dotCommaDouble: /[.,][.,]/i,
+      operatorDouble: /[+/:÷*×−-][+/:÷*×−-]/i,
+      dotCommaOperation: /[.,][+/:÷*×−-]/i,
+      operatorDotComma: /[+/:÷*×−-][.,]/i,
+    };
+    const state = this.state.mathExpression;
+    let isError = false;
+    if (state.match(/[/:÷]0/i) && !state.match(/[/:÷]0[.,]\d{0,}/i)) {
+      this.setState({
+        mathExpressionError: "Деление на ноль запрещено",
+      });
+      return false;
+    }
+    if (state === "" || state.length < 3 || !state.match(/[+/:÷*×−-]/i)) {
+      isError = true;
+    }
+    for (const key in reg) {
+      if (state.match(reg[key])) {
+        isError = true;
+      }
+    }
+    if (isError) {
+      this.setState({
+        mathExpressionError: "Некоректное выражение",
+      });
+      return false;
+    } else {
+      this.setState({
+        mathExpressionError: "",
+      });
+      return true;
+    }
+  }
+  handleChange = (event) => {
+    this.resetError();
+    this.setState({ mathExpression: event.currentTarget.value });
   };
-  handleClick(value) {
+  calculateExpression = () => {
     const history = this.state.history.slice();
+    const validateResult = this.validateMathExpression();
+    let result = null;
+    if (!validateResult) {
+      return;
+    } else {
+      result = calculate(this.state.mathExpression);
+      if (!isFinite(result)) {
+        this.setState({
+          mathExpressionError: "Неизвестная ошибка",
+        });
+        return;
+      }
+    }
+    this.setState((state) => ({
+      history: [
+        ...history,
+        {
+          mathExpression: state.mathExpression,
+          result,
+        },
+      ],
+      historyStep: state.historyStep + 1,
+      mathExpression: `${result}`,
+    }));
+  };
+  resetError = () => {
+    if (this.state.mathExpressionError !== "") {
+      this.setState({
+        mathExpressionError: "",
+      });
+    }
+  };
+
+  handleClick = (value) => {
     switch (value) {
       case "C":
-        let mathExpression = this.state.mathExpression.split("");
-        mathExpression.pop();
-        mathExpression = mathExpression.join("");
-        this.setState({
-          mathExpression: `${mathExpression}`,
-        });
+        this.resetError();
+        this.setState({ mathExpression: "" });
         break;
       case "=":
-        const result = calculate(this.state.mathExpression);
-        this.setState((state) => ({
-          history: [
-            ...history,
-            {
-              mathExpression: state.mathExpression,
-              result,
-            },
-          ],
-          mathExpression: `${result}`,
-        }));
+        this.calculateExpression();
         break;
       case "±":
         break;
       case "%":
         break;
-      case ".":
-        break;
       default:
+        this.resetError();
         this.setState((state) => ({
           mathExpression: `${state.mathExpression}${value}`,
         }));
         break;
     }
-  }
+  };
+  handleClickArrow = (event) => {
+    const historyStep = this.state.historyStep;
+    const history = this.state.history;
+    if (event.target.matches(".button-arrow-left")) {
+      const historyStepBack = historyStep - 1;
+      if (historyStepBack >= 0) {
+        this.setState({
+          historyStep: historyStepBack,
+          mathExpression: history[historyStepBack].result,
+        });
+      }
+    } else if (event.target.matches(".button-arrow-right")) {
+      const historyStepNext = historyStep + 1;
+      if (historyStepNext < history.length) {
+        this.setState({
+          historyStep: historyStepNext,
+          mathExpression: history[historyStepNext].result,
+        });
+      }
+    }
+  };
   render() {
-    const lastMathExpression =
-      this.state.history[this.state.history.length - 1].mathExpression;
+    const history = this.state.history;
+    const currentMathExpession = history[this.state.historyStep].mathExpression;
     return (
       <div className="calculator">
-        <p className="calculator__previous-result">{lastMathExpression}</p>
+        <ButtonsArrow handleClick={this.handleClickArrow}></ButtonsArrow>
+        {this.state.mathExpressionError === "" ? null : (
+          <MessageErrorExpression message={this.state.mathExpressionError} />
+        )}
+        <p className="calculator__previous-result">{currentMathExpession}</p>
         <InputField
           handleChange={this.handleChange}
           value={this.state.mathExpression}
+          calculateExpression={this.calculateExpression}
+          resetError={this.resetError}
         ></InputField>
         <div className="calculator__buttons">
           {this.numbers.map((number) => {
@@ -111,7 +214,7 @@ class Calculator extends Component {
               <ButtonNum
                 num={number}
                 key={number}
-                handleClick={(num) => this.handleClick(num)}
+                handleClick={this.handleClick}
               ></ButtonNum>
             );
           })}
@@ -122,7 +225,7 @@ class Calculator extends Component {
                   key={operation}
                   operation={operation}
                   className="button-equals"
-                  handleClick={(operation) => this.handleClick(operation)}
+                  handleClick={this.handleClick}
                 ></ButtonOperation>
               );
             }
@@ -130,7 +233,7 @@ class Calculator extends Component {
               <ButtonOperation
                 key={operation}
                 operation={operation}
-                handleClick={(operation) => this.handleClick(operation)}
+                handleClick={this.handleClick}
               ></ButtonOperation>
             );
           })}
